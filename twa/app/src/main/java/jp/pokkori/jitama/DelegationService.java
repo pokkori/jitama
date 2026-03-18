9b10499c97df700585d47f5650a580811e67bce1
pokkori <reborna2017@yahoo.co.jp>
2026-03-18 23:21:32 +0900

    feat: LINEシェアボタン追加

---
+++ twa/app/src/main/java/jp/pokkori/jitama/DelegationService.java
@@ -0,0 +1,14 @@
+package jp.pokkori.jitama;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
